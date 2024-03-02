2171ddca6c25e0d756ff2811bd3f9893dd21c8c9
Fredick Njeri <fredricknjeri64@gmail.com>
2024-03-02 11:41:36 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/getxguide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getxguide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/login/login_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: 
+      Text("Welcome to Home🔥"),),
+    );
+  }
+}
